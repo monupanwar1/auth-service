0219f07ed5e2c9abce55c31c0fb926343560c5ab
monupanwar1 <monupanwar5655@gmail.com>
2026-07-16 11:30:32 +0530

    add more test for tenant

--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 import app from "../../src/app";
 import { AppDataSource } from "../../src/config/data-source";
 import { Roles } from "../../src/constants";
+import { Tenant } from "../../src/entity/Tenant";
 
 jest.setTimeout(30000);
 describe("POST /tenants", () => {
@@ -48,11 +49,79 @@ describe("POST /tenants", () => {
         .post("/tenants")
         .set("Cookie", [`accessToken=${adminToken}`])
         .send(tenantData);
-      console.log(response.statusCode);
-      console.log(response.body);
 
       //assert
       expect(response.statusCode).toBe(201);
+    });
+    it("Should create a tenant in database", async () => {
+      //  Arrange
+      const tenantData = {
+        name: "Tenant name",
+        address: "Tenant address",
+      };
+      // act
+      const response = await request(app)
+        .post("/tenants")
+        .set("Cookie", [`accessToken=${adminToken}`])
+        .send(tenantData);
+
+      const tenantRepository = connection.getRepository(Tenant);
+
+      const tenants = await tenantRepository.find();
+
+      //assert
+
+      expect(tenants).toHaveLength(1);
+      expect(tenants[0].name).toBe(tenantData.name);
+      expect(tenants[0].address).toBe(tenantData.address);
+
+      expect(response.statusCode).toBe(201);
+    });
+    it("Should  return status code 401 if user is not authenticated", async () => {
+      //  Arrange
+      const tenantData = {
+        name: "Tenant name",
+        address: "Tenant address",
+      };
+      // act
+      const response = await request(app).post("/tenants").send(tenantData);
+
+      const tenantRepository = connection.getRepository(Tenant);
+
+      const tenants = await tenantRepository.find();
+
+      //assert
+
+      expect(tenants).toHaveLength(0);
+
+      expect(response.statusCode).toBe(401);
+    });
+    it("Should return status code 403 if user is not a admin", async () => {
+      //  Arrange
+      const managerToken = jwks.token({
+        sub: "1",
+        role: Roles.Manager,
+      });
+
+      const tenantData = {
+        name: "Tenant name",
+        address: "Tenant address",
+      };
+      // act
+      const response = await request(app)
+        .post("/tenants")
+        .set("Cookie", [`accessToken=${managerToken}`])
+        .send(tenantData);
+
+      expect(response.statusCode).toBe(403);
+
+      const tenantRepository = connection.getRepository(Tenant);
+
+      const tenants = await tenantRepository.find();
+
+      //assert
+
+      expect(tenants).toHaveLength(0);
     });
   });
 });
